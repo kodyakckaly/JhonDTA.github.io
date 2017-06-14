86d01fdc49b9db1463bae1533d81781a7cdf34bf
Jhonatan Daniel Tovar Arreguin <jhond.tovara@gmail.com>
2017-06-13 19:14:13 -0500

    Enables mailto

---
+++ mail/contact_me.php
@@ -0,0 +1,22 @@
+<?php
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+
+$to = 'mrpandait.business@gmail.com';
+$email_subject = "Contact MR. PANDA IT";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers = "From: mrpandait.business@gmail.com\n";
+$headers .= "Reply-To: $email_address";
+mail($to,$email_subject,$email_body,$headers);
+return true;
+?>
